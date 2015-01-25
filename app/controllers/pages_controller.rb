1aa88624da1d28c4d248f2018e1769837f11f1d0
Vladimir Motsak <vmotsak@gmail.com>
2015-01-25 16:20:00 +0100

    set guest address

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,7 +14,7 @@ class PagesController < ApplicationController
       @events = @events.where(events: {language_id: @search_form.language_id}) if @search_form.language_id.present?
       @events = @events.map { |l| l.events if l.events.present? }.compact
     else
-      @search_form = SearchParams.new(language_id: '', radius: 10, address: current_address)
+      @search_form = SearchParams.new(language_id: '', radius: 100, address: current_address)
     end
   end
 
@@ -24,7 +24,7 @@ class PagesController < ApplicationController
     if current_user
       current_user.location.address
     else
-      request.location.address
+      'New York, USA'
     end
   end
 
