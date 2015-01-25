88b451edeff5001d014b4b4d0eaaa489b9b54952
Vladimir Motsak <vmotsak@gmail.com>
2015-01-25 09:41:28 +0100

    user name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :reject_locked!, if: :devise_controller?
 
@@ -13,25 +13,29 @@ class ApplicationController < ActionController::Base
   # Devise permitted params
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(
-      :email, 
-      :password, 
-      :password_confirmation,
-      :address) 
+        :email,
+        :password,
+        :password_confirmation,
+        :address,
+        :first_name,
+        :last_name)
     }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
-      :email, 
-      :password, 
-      :password_confirmation,
-      :current_password,
-      :address) 
+        :email,
+        :password,
+        :password_confirmation,
+        :current_password,
+        :address,
+        :first_name,
+        :last_name)
     }
   end
-  
+
   # Redirects on successful sign in
   def after_sign_in_path_for(resource)
     root_path
   end
-  
+
   # Auto-sign out locked users
   def reject_locked!
     if current_user && current_user.locked?
@@ -43,16 +47,18 @@ class ApplicationController < ActionController::Base
       redirect_to root_url
     end
   end
+
   helper_method :reject_locked!
-  
+
   # Only permits admin users
   def require_admin!
     authenticate_user!
-    
+
     if current_user && !current_user.admin?
       redirect_to root_path
     end
   end
+
   helper_method :require_admin!
-  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150124151959) do
+ActiveRecord::Schema.define(version: 20150125082856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,8 @@ ActiveRecord::Schema.define(version: 20150124151959) do
     t.datetime "updated_at"
     t.string   "address"
     t.integer  "location_id"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
